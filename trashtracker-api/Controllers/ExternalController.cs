31a337adb2b7fd8fa92eb2fb16d0e6a6f7296419
Niels Jansen <niels.jansen@schippers.eu>
2025-06-17 13:18:28 +0200

    Expanded the prediction API

--- trashtracker-api/Controllers/ExternalController.cs
+++ trashtracker-api/Controllers/ExternalController.cs
@@ -15,10 +15,28 @@ namespace trashtracker_api.Controllers
         [Authorize]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<Prediction>> GetPredictionForDate([FromQuery] DateOnly StartDate, [FromQuery] DateOnly EndDate)
+        public async Task<ActionResult<Prediction>> GetPredictionForDate([FromQuery] DateOnly startDate, [FromQuery] DateOnly endDate)
         {
-            // TO DO: Schrijf iets om predictions te maken
-            return null;
+            if (startDate > endDate)
+            {
+                return BadRequest("Start date must be earlier than or equal to the end date.");
+            }
+
+            var client = new HttpClient();
+            string url = $"https://localhost:7200/{startDate.Year}/NL";
+
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var prediction = await response.Content.ReadFromJsonAsync<Prediction>();
+                return Ok(prediction);
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+                return StatusCode((int)response.StatusCode, "API call failed");
+            }
         }
 
         // Getting Holiday Data
@@ -41,7 +59,7 @@ namespace trashtracker_api.Controllers
             if (response.IsSuccessStatusCode)
             {
                 var holidays = await response.Content.ReadFromJsonAsync<List<Holiday>>();
-                return holidays;
+                return Ok(holidays);
             }
             else
             {
