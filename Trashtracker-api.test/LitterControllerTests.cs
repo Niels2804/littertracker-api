59b5329b1c6dda7d6bd499d81b799390a13c68b6
mikeyboy52 <mikevanlommel2007@gmail.com>
2025-06-19 12:53:52 +0200

    Unit testen gefixed, en gemiste getest

--- Trashtracker-api.test/LitterControllerTests.cs
+++ Trashtracker-api.test/LitterControllerTests.cs
@@ -90,5 +90,105 @@ namespace Trashtracker_api.test
             // Assert
             Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult));
         }
+
+        [TestMethod]
+        public async Task CreateLitter_ValidLitter_ReturnsCreated()
+        {
+            var newLitter = new Litter { Id = Guid.NewGuid().ToString() };
+
+            _litterRepository.Setup(r => r.GetByLitterIdAsync(newLitter.Id)).ReturnsAsync((Litter)null);
+            _litterRepository.Setup(r => r.CreateLitterAsync(newLitter)).ReturnsAsync(newLitter);
+
+            var result = await _controller.CreateLitter(newLitter);
+
+            var createdResult = result.Result as CreatedAtRouteResult;
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual(201, createdResult.StatusCode);
+            Assert.AreEqual(newLitter, createdResult.Value);
+        }
+
+        [TestMethod]
+        public async Task CreateLitter_ExistingLitter_ReturnsBadRequest()
+        {
+            var existingLitter = new Litter { Id = Guid.NewGuid().ToString() };
+
+            _litterRepository.Setup(r => r.GetByLitterIdAsync(existingLitter.Id)).ReturnsAsync(existingLitter);
+
+            var result = await _controller.CreateLitter(existingLitter);
+
+            var badRequest = result.Result as BadRequestObjectResult;
+            Assert.IsNotNull(badRequest);
+            Assert.AreEqual(400, badRequest.StatusCode);
+            Assert.AreEqual("Litter with this ID already exists.", badRequest.Value);
+        }
+
+
+        [TestMethod]
+        public async Task UpdateLitter_ValidRequest_ReturnsOk()
+        {
+            var litterId = Guid.NewGuid().ToString();
+            var updatedLitter = new Litter { Id = litterId };
+
+            _litterRepository.Setup(r => r.GetByLitterIdAsync(litterId)).ReturnsAsync(updatedLitter);
+
+            var result = await _controller.UpdateLitter(litterId, updatedLitter);
+
+            var ok = result.Result as OkObjectResult;
+            Assert.IsNotNull(ok);
+            Assert.AreEqual(updatedLitter, ok.Value);
+        }
+
+        [TestMethod]
+        public async Task UpdateLitter_LitterNotFound_ReturnsNotFound()
+        {
+            var litterId = Guid.NewGuid().ToString();
+            var updatedLitter = new Litter { Id = litterId };
+
+            _litterRepository.Setup(r => r.GetByLitterIdAsync(litterId)).ReturnsAsync((Litter)null);
+
+            var result = await _controller.UpdateLitter(litterId, updatedLitter);
+
+            Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task UpdateLitter_IdMismatch_ReturnsBadRequest()
+        {
+            var litterId = Guid.NewGuid().ToString();
+            var differentId = Guid.NewGuid().ToString();
+            var litter = new Litter { Id = differentId };
+
+            var result = await _controller.UpdateLitter(litterId, litter);
+
+            var badRequest = result.Result as BadRequestObjectResult;
+            Assert.IsNotNull(badRequest);
+            Assert.AreEqual("Litter data is required or ID mismatch.", badRequest.Value);
+        }
+
+        [TestMethod]
+        public async Task DeleteLitter_Exists_ReturnsNoContent()
+        {
+            var litterId = Guid.NewGuid().ToString();
+            var litter = new Litter { Id = litterId };
+
+            _litterRepository.Setup(r => r.GetByLitterIdAsync(litterId)).ReturnsAsync(litter);
+            _litterRepository.Setup(r => r.DeleteLitterAsync(litterId)).Returns(Task.CompletedTask);
+
+            var result = await _controller.DeleteLitter(litterId);
+
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
+        }
+
+        [TestMethod]
+        public async Task DeleteLitter_NotFound_ReturnsNotFound()
+        {
+            var litterId = Guid.NewGuid().ToString();
+
+            _litterRepository.Setup(r => r.GetByLitterIdAsync(litterId)).ReturnsAsync((Litter)null);
+
+            var result = await _controller.DeleteLitter(litterId);
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
     }
 }
